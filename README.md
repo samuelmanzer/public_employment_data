c857e3c60f26c227b6968b45eb08b943a2c84e5c
Sam Manzer <samuelmanzer@users.noreply.github.com>
2017-05-16 19:06:37 -0500

    add decompression

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ virtualenv env
 pip install -r requirements.txt
 ```
 
-Now you can pull Federal employment data from OPM:  
+Now you can pull Federal employment data from OPM; the script will auto-decompress
+the data for you:
 
 ```
 python scripts/fetch_raw_data.py
--- scripts/fetch_raw_data.py
+++ scripts/fetch_raw_data.py
@@ -3,17 +3,24 @@
 import requests
 import os
 
+from zipfile import ZipFile
+
 # We need to get some cookies before we directly 
 # our target file
 establish_session_req = requests.get('https://www.opm.gov/data/')
-
 data_file_req = requests.get('https://www.opm.gov/Data/Files/425/fdb8db0d-ce56-4966-9572-9692e6e0fa5e.zip')
 
+# Create gitignored raw data path if nonexistent
 data_file_dir = './data/fedscope/2016_03/'
 if not os.path.exists(data_file_dir):
     os.makedirs(data_file_dir)
 
+# Write the zip file that was sent in the response 
 data_file_path = data_file_dir + 'raw.zip'
 data_file = open(data_file_path, 'wb')
 data_file.write(data_file_req.content)
 data_file.close()
+
+# Decompress the zip file 
+zf = ZipFile(data_file_path)
+zf.extractall(data_file_dir)
